e19ad2a2eafe0f7dcc6517133c32b550d9d9ccc7
Aaron Tobias <tobias613.aaron@gmail.com>
2018-01-18 16:32:45 -0500

    Added wallet cookie caching behavior

--- src/__tests__/reducers/balance.js
+++ src/__tests__/reducers/balance.js
@@ -1,39 +1,50 @@
 import balanceReducer from '../../reducers/balance';
+import balanceCacheReducer from '../../reducers/balance'
 import * as constants from '../../actions/constants';
 
 describe('balance reducer', () => {
-    it('sets a balance', () => {
-        const balance = 10;
+    const balance = 10;
 
-        const actionData = { type: constants.SET_BALANCE, balance };
+    describe('when initializing', () => {
+        it('sets a balance', () => {
+            const actionData = {type: constants.SET_BALANCE, balance};
 
-        expect(balanceReducer(undefined, actionData)).toEqual(balance)
-    })
+            expect(balanceReducer(undefined, actionData)).toEqual(balance)
+        });
+
+        describe('then re-initializing', () => {
+            it('reads the balance from cookies', () => {
+                // Same reducer as balanceReducer; renamed on import
+                // Note: this depends on the previous test to run before
+                // this test to set the initial balance - bad practise
+                // TODO: make this test independent
+                expect(balanceCacheReducer(undefined, {})).toEqual(balance)
+            });
+        });
+    });
 
     it('deposits into the balance', () => {
         const deposit = 10;
-        const initialData = 5;
-
         const actionData = { type: constants.DEPOSIT, deposit };
-        expect(balanceReducer(initialData, actionData)).toEqual(deposit + initialData)
-    })
+
+        expect(balanceReducer(balance, actionData)).toEqual(deposit + balance)
+    });
 
     describe('withdrawal', () => {
         
         const withdrawal = 10;
 
         it('occurs when there is enouph balance', () => {
-            const initialData = 10;
-
             const actionData = {type: constants.WITHDRAW, withdrawal};
-            expect(balanceReducer(initialData, actionData)).toEqual(initialData - withdrawal)
-        })
+
+            expect(balanceReducer(balance, actionData)).toEqual(balance - withdrawal)
+        });
 
         it('does not occur when there is not enough balance', () => {
             const initialData = 5;
-
             const actionData = {type: constants.WITHDRAW, withdrawal};
+
             expect(balanceReducer(initialData, actionData)).toEqual(initialData)
         })
     })
-})
+});
--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -1,17 +1,28 @@
-import * as constants from '../actions/constants'
+import * as constants from '../actions/constants';
+import { read_cookie, bake_cookie } from 'sfcookies';
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE';
 
 const balance = (state = 0, action) => {
+    let balance;
+
     switch(action.type) {
         case constants.SET_BALANCE:
-            return action.balance;
+            balance = action.balance;
+            break;
         case constants.DEPOSIT:
-            return state + action.deposit;
+            balance = state + action.deposit;
+            break;
         case constants.WITHDRAW:
-            const diff = state - action.withdrawal;
-            return (diff >= 0) ? diff : state
+            balance = state - action.withdrawal;
+            balance = (balance >= 0) ? balance : state;
+            break;
         default:
-            return state;
+            balance = parseInt(read_cookie(BALANCE_COOKIE, 10)) || state;
     }
+
+    bake_cookie(BALANCE_COOKIE, balance);
+    return balance;
 };
 
 export default balance;
