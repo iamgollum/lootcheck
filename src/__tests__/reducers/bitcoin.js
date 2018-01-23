09859c9624059097a14e80ce08aea9a263b27234
Aaron Tobias <tobias613.aaron@gmail.com>
2018-01-22 21:06:45 -0500

    Applied redux thunk middleware and combined reducers

---
+++ src/__tests__/reducers/bitcoin.js
@@ -0,0 +1,10 @@
+import bitcoinReducer from '../../reducers/bitcoin';
+import { FETCH_BITCOIN } from "../../actions/constants";
+
+describe('bitcoinReducer', () => {
+    const bitcoinData = { bpi: 'bitcoin price index' }
+
+    it('fetches and sets the bitcoin data', () => {
+        bitcoinReducer({}, { type: FETCH_BITCOIN, bitcoin: bitcoinData })
+    });
+});
